d334d3909ed449ff744be43adafcda2b5a43473d
vemerion <vemerion@gmail.com>
2020-11-29 14:45:14 +0100

    Add brontosaurus AI tasks

--- src/main/java/mod/vemerion/vemerioraptor/entity/BrontosaurusEntity.java
+++ src/main/java/mod/vemerion/vemerioraptor/entity/BrontosaurusEntity.java
@@ -1,17 +1,39 @@
 package mod.vemerion.vemerioraptor.entity;
 
+import java.util.UUID;
+
 import mod.vemerion.vemerioraptor.Main;
 import net.minecraft.entity.AgeableEntity;
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.IAngerable;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.MobEntity;
 import net.minecraft.entity.ai.attributes.AttributeModifierMap;
 import net.minecraft.entity.ai.attributes.Attributes;
+import net.minecraft.entity.ai.goal.BreedGoal;
+import net.minecraft.entity.ai.goal.HurtByTargetGoal;
+import net.minecraft.entity.ai.goal.LookAtGoal;
+import net.minecraft.entity.ai.goal.LookRandomlyGoal;
+import net.minecraft.entity.ai.goal.MeleeAttackGoal;
+import net.minecraft.entity.ai.goal.NearestAttackableTargetGoal;
+import net.minecraft.entity.ai.goal.ResetAngerGoal;
+import net.minecraft.entity.ai.goal.SwimGoal;
+import net.minecraft.entity.ai.goal.WaterAvoidingRandomWalkingGoal;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.tags.ItemTags;
+import net.minecraft.util.RangedInteger;
+import net.minecraft.util.TickRangeConverter;
 import net.minecraft.world.World;
 import net.minecraft.world.server.ServerWorld;
 
-public class BrontosaurusEntity extends DinosaurEntity {
+public class BrontosaurusEntity extends DinosaurEntity implements IAngerable {
+
+	private static final RangedInteger ANGRY_RANGE = TickRangeConverter.convertRange(20, 39);
+
+	private int angryTime;
+	private UUID angerTarget;
 
 	public BrontosaurusEntity(EntityType<? extends BrontosaurusEntity> type, World worldIn) {
 		super(type, worldIn);
@@ -27,6 +49,14 @@ public class BrontosaurusEntity extends DinosaurEntity {
 				.createMutableAttribute(Attributes.ATTACK_DAMAGE, 2.0D);
 	}
 
+	@Override
+	public void livingTick() {
+		super.livingTick();
+
+		if (!world.isRemote) {
+			this.func_241359_a_((ServerWorld) world, true); // IAngerable tick
+		}
+	}
 
 	@Override
 	public AgeableEntity func_241840_a(ServerWorld world, AgeableEntity parent) {
@@ -35,11 +65,63 @@ public class BrontosaurusEntity extends DinosaurEntity {
 
 	@Override
 	protected void registerGoals() {
+		goalSelector.addGoal(0, new SwimGoal(this));
+		goalSelector.addGoal(1, new MeleeAttackGoal(this, 1.0D, true) {
+			@Override
+			protected double getAttackReachSqr(LivingEntity attackTarget) {
+				return super.getAttackReachSqr(attackTarget) * 0.5;
+			}
+		});
+		goalSelector.addGoal(3, new BreedGoal(this, 1));
+		goalSelector.addGoal(6, new WaterAvoidingRandomWalkingGoal(this, 0.7D));
+		goalSelector.addGoal(7, new LookAtGoal(this, PlayerEntity.class, 6.0F));
+		goalSelector.addGoal(8, new LookRandomlyGoal(this));
+		targetSelector.addGoal(3, new HurtByTargetGoal(this));
+		targetSelector.addGoal(4,
+				new NearestAttackableTargetGoal<>(this, PlayerEntity.class, 10, true, false, this::func_233680_b_));
+		targetSelector.addGoal(8, new ResetAngerGoal<>(this, true));
 
 	}
-	
+
+	@Override
+	public void writeAdditional(CompoundNBT compound) {
+		super.writeAdditional(compound);
+		writeAngerNBT(compound);
+	}
+
+	@Override
+	public void readAdditional(CompoundNBT compound) {
+		super.readAdditional(compound);
+		readAngerNBT((ServerWorld) world, compound);
+	}
+
 	@Override
 	public boolean isBreedingItem(ItemStack stack) {
 		return ItemTags.LEAVES.contains(stack.getItem());
+	}
+
+	@Override
+	public int getAngerTime() {
+		return angryTime;
+	}
+
+	@Override
+	public void setAngerTime(int time) {
+		angryTime = time;
+	}
+
+	@Override
+	public UUID getAngerTarget() {
+		return angerTarget;
+	}
+
+	@Override
+	public void setAngerTarget(UUID target) {
+		angerTarget = target;
+	}
+
+	@Override
+	public void func_230258_H__() {
+		setAngerTime(ANGRY_RANGE.getRandomWithinRange(rand));
 	}
 }
--- src/main/java/mod/vemerion/vemerioraptor/model/BrontosaurusModel.java
+++ src/main/java/mod/vemerion/vemerioraptor/model/BrontosaurusModel.java
@@ -13,107 +13,126 @@ import net.minecraft.util.ResourceLocation;
  * Created using Tabula 8.0.0
  */
 public class BrontosaurusModel extends DinosaurModel<BrontosaurusEntity> {
-	
-	private static final ResourceLocation TEXTURES = new ResourceLocation(Main.MODID, "textures/entity/brontosaurus.png");
 
-	
-    public ModelRenderer body;
-    public ModelRenderer tail1;
-    public ModelRenderer backLeftLeg;
-    public ModelRenderer frontLeftLeg;
-    public ModelRenderer neck1;
-    public ModelRenderer tail2;
-    public ModelRenderer tail3;
-    public ModelRenderer tail4;
-    public ModelRenderer tail5;
-    public ModelRenderer neck2;
-    public ModelRenderer neck3;
-    public ModelRenderer neck4;
-    public ModelRenderer head;
+	private static final ResourceLocation TEXTURES = new ResourceLocation(Main.MODID,
+			"textures/entity/brontosaurus.png");
 
-    public BrontosaurusModel() {
-        this.textureWidth = 256;
-        this.textureHeight = 256;
-        this.tail3 = new ModelRenderer(this, 64, 16);
-        this.tail3.setRotationPoint(0.0F, 0.0F, 15.0F);
-        this.tail3.setTextureOffset(138, 0).addBox(-5.0F, -5.0F, 0.0F, 10.0F, 10.0F, 14.0F, 0.0F, 0.0F, 0.0F);
-        this.setRotateAngle(tail3, 0.11728612207217244F, 0.0F, 0.0F);
-        this.head = new ModelRenderer(this, 64, 32);
-        this.head.setRotationPoint(0.0F, -3.0F, -18.0F);
-        this.head.setTextureOffset(84, 34).addBox(-4.0F, -4.0F, -14.0F, 8.0F, 8.0F, 14.0F, 0.0F, 0.0F, 0.0F);
-        this.setRotateAngle(head, 1.1728612040769677F, 0.0F, 0.0F);
-        this.body = new ModelRenderer(this, 0, 0);
-        this.body.setRotationPoint(0.0F, -2.0F, 0.0F);
-        this.body.addBox(-12.0F, -12.0F, -18.0F, 24.0F, 24.0F, 36.0F, 0.0F, 0.0F, 0.0F);
-        this.tail2 = new ModelRenderer(this, 64, 0);
-        this.tail2.setRotationPoint(0.0F, 0.0F, 16.0F);
-        this.tail2.setTextureOffset(92, 0).addBox(-7.0F, -7.0F, 0.0F, 14.0F, 14.0F, 16.0F, 0.0F, 0.0F, 0.0F);
-        this.neck2 = new ModelRenderer(this, 64, 32);
-        this.neck2.setRotationPoint(0.0F, 0.0F, -16.0F);
-        this.neck2.setTextureOffset(130, 26).addBox(-5.0F, -5.0F, -18.0F, 10.0F, 10.0F, 18.0F, 0.0F, 0.0F, 0.0F);
-        this.setRotateAngle(neck2, -0.27366763203903305F, 0.0F, 0.0F);
-        this.neck3 = new ModelRenderer(this, 0, 32);
-        this.neck3.setRotationPoint(0.0F, 0.0F, -15.0F);
-        this.neck3.setTextureOffset(0, 28).addBox(-4.0F, -4.0F, -18.0F, 8.0F, 8.0F, 18.0F, 0.0F, 0.0F, 0.0F);
-        this.setRotateAngle(neck3, -0.4300491170387584F, 0.0F, 0.0F);
-        this.backLeftLeg = new ModelRenderer(this, 64, 30);
-        this.backLeftLeg.setRotationPoint(8.0F, -2.0F, 10.0F);
-        this.backLeftLeg.setTextureOffset(84, 0).addBox(0.0F, 0.0F, -4.0F, 8.0F, 28.0F, 8.0F, 0.0F, 0.0F, 0.0F);
-        this.tail4 = new ModelRenderer(this, 0, 0);
-        this.tail4.setRotationPoint(0.0F, 0.0F, 13.0F);
-        this.tail4.addBox(-3.0F, -3.0F, 0.0F, 6.0F, 6.0F, 10.0F, 0.0F, 0.0F, 0.0F);
-        this.setRotateAngle(tail4, 0.1563815016444822F, 0.0F, 0.0F);
-        this.tail5 = new ModelRenderer(this, 64, 0);
-        this.tail5.setRotationPoint(0.0F, 0.0F, 9.0F);
-        this.tail5.setTextureOffset(74, 0).addBox(-1.5F, -1.5F, 0.0F, 3.0F, 3.0F, 10.0F, 0.0F, 0.0F, 0.0F);
-        this.setRotateAngle(tail5, 0.11728612207217244F, 0.0F, 0.0F);
-        this.tail1 = new ModelRenderer(this, 64, 0);
-        this.tail1.setRotationPoint(0.0F, 0.0F, 16.0F);
-        this.tail1.setTextureOffset(20, 0).addBox(-9.0F, -9.0F, 0.0F, 18.0F, 18.0F, 18.0F, 0.0F, 0.0F, 0.0F);
-        this.setRotateAngle(tail1, -0.19547687289441354F, 0.0F, 0.0F);
-        this.neck4 = new ModelRenderer(this, 52, 32);
-        this.neck4.setRotationPoint(0.0F, 0.0F, -16.0F);
-        this.neck4.setTextureOffset(0, 28).addBox(-3.0F, -3.0F, -18.0F, 6.0F, 6.0F, 18.0F, 0.0F, 0.0F, 0.0F);
-        this.setRotateAngle(neck4, -0.19547687289441354F, 0.0F, 0.0F);
-        this.neck1 = new ModelRenderer(this, 64, 32);
-        this.neck1.setRotationPoint(0.0F, -4.0F, -16.0F);
-        this.neck1.setTextureOffset(38, 16).addBox(-6.0F, -6.0F, -18.0F, 12.0F, 12.0F, 18.0F, 0.0F, 0.0F, 0.0F);
-        this.setRotateAngle(neck1, 0.0781907508222411F, 0.0F, 0.0F);
-        this.frontLeftLeg = new ModelRenderer(this, 64, 32);
-        this.frontLeftLeg.setRotationPoint(8.0F, -2.0F, -10.0F);
-        this.frontLeftLeg.setTextureOffset(116, 8).addBox(0.0F, 0.0F, -4.0F, 8.0F, 28.0F, 8.0F, 0.0F, 0.0F, 0.0F);
-        this.tail2.addChild(this.tail3);
-        this.neck4.addChild(this.head);
-        this.tail1.addChild(this.tail2);
-        this.neck1.addChild(this.neck2);
-        this.neck2.addChild(this.neck3);
-        this.body.addChild(this.backLeftLeg);
-        this.tail3.addChild(this.tail4);
-        this.tail4.addChild(this.tail5);
-        this.body.addChild(this.tail1);
-        this.neck3.addChild(this.neck4);
-        this.body.addChild(this.neck1);
-        this.body.addChild(this.frontLeftLeg);
-    }
+	public ModelRenderer body;
+	public ModelRenderer tail1;
+	public ModelRenderer backLeftLeg;
+	public ModelRenderer frontLeftLeg;
+	public ModelRenderer neck1;
+	public ModelRenderer frontRightLeg;
+	public ModelRenderer backRightLeg;
+	public ModelRenderer tail2;
+	public ModelRenderer tail3;
+	public ModelRenderer tail4;
+	public ModelRenderer tail5;
+	public ModelRenderer neck2;
+	public ModelRenderer neck3;
+	public ModelRenderer neck4;
+	public ModelRenderer head;
 
-    @Override
-    public void render(MatrixStack matrixStackIn, IVertexBuilder bufferIn, int packedLightIn, int packedOverlayIn, float red, float green, float blue, float alpha) { 
-        ImmutableList.of(this.body).forEach((modelRenderer) -> { 
-            modelRenderer.render(matrixStackIn, bufferIn, packedLightIn, packedOverlayIn, red, green, blue, alpha);
-        });
-    }
+	public BrontosaurusModel() {
+		this.textureWidth = 256;
+		this.textureHeight = 256;
+		this.neck1 = new ModelRenderer(this, 64, 32);
+		this.neck1.setRotationPoint(0.0F, -4.0F, -16.0F);
+		this.neck1.setTextureOffset(38, 16).addBox(-6.0F, -6.0F, -18.0F, 12.0F, 12.0F, 18.0F, 0.0F, 0.0F, 0.0F);
+		this.setRotateAngle(neck1, 0.0781907508222411F, 0.0F, 0.0F);
+		this.frontLeftLeg = new ModelRenderer(this, 64, 32);
+		this.frontLeftLeg.setRotationPoint(8.0F, -2.0F, -10.0F);
+		this.frontLeftLeg.setTextureOffset(116, 8).addBox(0.0F, 0.0F, -4.0F, 8.0F, 28.0F, 8.0F, 0.0F, 0.0F, 0.0F);
+		this.tail2 = new ModelRenderer(this, 64, 0);
+		this.tail2.setRotationPoint(0.0F, 0.0F, 16.0F);
+		this.tail2.setTextureOffset(92, 0).addBox(-7.0F, -7.0F, 0.0F, 14.0F, 14.0F, 16.0F, 0.0F, 0.0F, 0.0F);
+		this.backRightLeg = new ModelRenderer(this, 64, 32);
+		this.backRightLeg.setRotationPoint(-8.0F, -2.0F, 10.0F);
+		this.backRightLeg.setTextureOffset(116, 8).addBox(-8.0F, 0.0F, -4.0F, 8.0F, 28.0F, 8.0F, 0.0F, 0.0F, 0.0F);
+		this.backLeftLeg = new ModelRenderer(this, 64, 30);
+		this.backLeftLeg.setRotationPoint(8.0F, -2.0F, 10.0F);
+		this.backLeftLeg.setTextureOffset(84, 0).addBox(0.0F, 0.0F, -4.0F, 8.0F, 28.0F, 8.0F, 0.0F, 0.0F, 0.0F);
+		this.tail3 = new ModelRenderer(this, 64, 16);
+		this.tail3.setRotationPoint(0.0F, 0.0F, 15.0F);
+		this.tail3.setTextureOffset(138, 0).addBox(-5.0F, -5.0F, 0.0F, 10.0F, 10.0F, 14.0F, 0.0F, 0.0F, 0.0F);
+		this.setRotateAngle(tail3, 0.11728612207217244F, 0.0F, 0.0F);
+		this.frontRightLeg = new ModelRenderer(this, 64, 32);
+		this.frontRightLeg.setRotationPoint(-8.0F, -2.0F, -10.0F);
+		this.frontRightLeg.setTextureOffset(116, 8).addBox(-8.0F, 0.0F, -4.0F, 8.0F, 28.0F, 8.0F, 0.0F, 0.0F, 0.0F);
+		this.neck2 = new ModelRenderer(this, 64, 32);
+		this.neck2.setRotationPoint(0.0F, 0.0F, -16.0F);
+		this.neck2.setTextureOffset(130, 26).addBox(-5.0F, -5.0F, -18.0F, 10.0F, 10.0F, 18.0F, 0.0F, 0.0F, 0.0F);
+		this.setRotateAngle(neck2, -0.27366763203903305F, 0.0F, 0.0F);
+		this.head = new ModelRenderer(this, 64, 32);
+		this.head.setRotationPoint(0.0F, -3.0F, -18.0F);
+		this.head.setTextureOffset(84, 34).addBox(-4.0F, -4.0F, -14.0F, 8.0F, 8.0F, 14.0F, 0.0F, 0.0F, 0.0F);
+		this.setRotateAngle(head, 1.1728612040769677F, 0.0F, 0.0F);
+		this.tail5 = new ModelRenderer(this, 64, 0);
+		this.tail5.setRotationPoint(0.0F, 0.0F, 9.0F);
+		this.tail5.setTextureOffset(74, 0).addBox(-1.5F, -1.5F, 0.0F, 3.0F, 3.0F, 10.0F, 0.0F, 0.0F, 0.0F);
+		this.setRotateAngle(tail5, 0.11728612207217244F, 0.0F, 0.0F);
+		this.neck3 = new ModelRenderer(this, 0, 32);
+		this.neck3.setRotationPoint(0.0F, 0.0F, -15.0F);
+		this.neck3.setTextureOffset(0, 28).addBox(-4.0F, -4.0F, -18.0F, 8.0F, 8.0F, 18.0F, 0.0F, 0.0F, 0.0F);
+		this.setRotateAngle(neck3, -0.4300491170387584F, 0.0F, 0.0F);
+		this.neck4 = new ModelRenderer(this, 52, 32);
+		this.neck4.setRotationPoint(0.0F, 0.0F, -16.0F);
+		this.neck4.setTextureOffset(0, 28).addBox(-3.0F, -3.0F, -18.0F, 6.0F, 6.0F, 18.0F, 0.0F, 0.0F, 0.0F);
+		this.setRotateAngle(neck4, -0.19547687289441354F, 0.0F, 0.0F);
+		this.tail4 = new ModelRenderer(this, 0, 0);
+		this.tail4.setRotationPoint(0.0F, 0.0F, 13.0F);
+		this.tail4.addBox(-3.0F, -3.0F, 0.0F, 6.0F, 6.0F, 10.0F, 0.0F, 0.0F, 0.0F);
+		this.setRotateAngle(tail4, 0.1563815016444822F, 0.0F, 0.0F);
+		this.tail1 = new ModelRenderer(this, 64, 0);
+		this.tail1.setRotationPoint(0.0F, 0.0F, 16.0F);
+		this.tail1.setTextureOffset(20, 0).addBox(-9.0F, -9.0F, 0.0F, 18.0F, 18.0F, 18.0F, 0.0F, 0.0F, 0.0F);
+		this.setRotateAngle(tail1, -0.19547687289441354F, 0.0F, 0.0F);
+		this.body = new ModelRenderer(this, 0, 0);
+		this.body.setRotationPoint(0.0F, -2.0F, 0.0F);
+		this.body.addBox(-12.0F, -12.0F, -18.0F, 24.0F, 24.0F, 36.0F, 0.0F, 0.0F, 0.0F);
+		this.body.addChild(this.neck1);
+		this.body.addChild(this.frontLeftLeg);
+		this.tail1.addChild(this.tail2);
+		this.body.addChild(this.backRightLeg);
+		this.body.addChild(this.backLeftLeg);
+		this.tail2.addChild(this.tail3);
+		this.body.addChild(this.frontRightLeg);
+		this.neck1.addChild(this.neck2);
+		this.neck4.addChild(this.head);
+		this.tail4.addChild(this.tail5);
+		this.neck2.addChild(this.neck3);
+		this.neck3.addChild(this.neck4);
+		this.tail3.addChild(this.tail4);
+		this.body.addChild(this.tail1);
+	}
 
-    @Override
-    public void setRotationAngles(BrontosaurusEntity entityIn, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch) {}
+	@Override
+	public void render(MatrixStack matrixStackIn, IVertexBuilder bufferIn, int packedLightIn, int packedOverlayIn,
+			float red, float green, float blue, float alpha) {
+		ImmutableList.of(this.body).forEach((modelRenderer) -> {
+			modelRenderer.render(matrixStackIn, bufferIn, packedLightIn, packedOverlayIn, red, green, blue, alpha);
+		});
+	}
 
-    /**
-     * This is a helper function from Tabula to set the rotation of model parts
-     */
-    public void setRotateAngle(ModelRenderer modelRenderer, float x, float y, float z) {
-        modelRenderer.rotateAngleX = x;
-        modelRenderer.rotateAngleY = y;
-        modelRenderer.rotateAngleZ = z;
-    }
+	@Override
+	public void setRotationAngles(BrontosaurusEntity entityIn, float limbSwing, float limbSwingAmount, float ageInTicks,
+			float netHeadYaw, float headPitch) {
+	}
+
+	@Override
+	public void setLivingAnimations(BrontosaurusEntity entityIn, float limbSwing, float limbSwingAmount,
+			float partialTick) {
+
+	}
+
+	/**
+	 * This is a helper function from Tabula to set the rotation of model parts
+	 */
+	public void setRotateAngle(ModelRenderer modelRenderer, float x, float y, float z) {
+		modelRenderer.rotateAngleX = x;
+		modelRenderer.rotateAngleY = y;
+		modelRenderer.rotateAngleZ = z;
+	}
 
 	@Override
 	public ResourceLocation getTexture() {
